67d4b3dd3ac6ed8767a710a31efe68da56d5014c
Eric Taylor <eric.taylor1012@gmail.com>
2022-10-06 03:57:36 -0700

    starting html and css

---
+++ index.js
@@ -0,0 +1,9 @@
+const loginButton = document.getElementById("login-button");
+
+
+function login() {
+    location.href = "https://www.google.com"
+}
+
+
+loginButton.addEventListener("click", login)
