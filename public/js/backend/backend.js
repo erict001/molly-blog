d6c4067330cca473f8c83a94e2f38545937f7787
Eric Taylor <eric.taylor1012@gmail.com>
2022-12-22 22:35:59 -0800

    restyled add blog to get an accordion and better functionality

--- public/js/backend/backend.js
+++ public/js/backend/backend.js
@@ -10,16 +10,25 @@ const blogDescription = document.getElementById("blog-description")
 // add media to SQL database
 const media = document.getElementById("add-media")
 
-//STYLING blog_content_1
+//allowing change to adjust blog_content based on user selection
+const blogSelector = document.getElementById("contentSelector")
+const blogButton = document.getElementById("blogButton")
+const blogChoice = ["bg1", "bg2", "bg3", "bg4", "bg5"]
 
 //text decoration
 const bold = document.getElementById("bold")
 const italic = document.getElementById("italic")
 const underline = document.getElementById("underline")
+
 //font changes
 const fontSize = document.getElementById("font-size")
 const fontFamily = document.getElementById("font-families")
 const fontChange = document.getElementById("changeText")
+
+//add list elements
+const headerFont = document.getElementById("headerFont")
+const headers = document.getElementById("header-style")
+const headerArray = ["xx-large", "x-large", "large", "medium", "small", "12px"]
 
 // allows us to change value of blog content
 const fontArray = ["arial", "verdana", "tahoma", "times-new-roman", "courier-new"]
@@ -27,18 +36,15 @@ const fontArray = ["arial", "verdana", "tahoma", "times-new-roman", "courier-new
 const pageChange = document.getElementById("pageChange")
 const submitBlog = document.getElementById("submit")
 
-
 const text = document.getElementById("addText")
 
 
-//DOWNLOAD IMAGE URL FROM AWS FROM LOCAL MACHINE
-
-//
+//DOWNLOAD IMAGE URL FROM AWS FROM LOCAL MACHINE//
 const imageUrl = []
 console.log(imageUrl)
 
 //event listener to activate button
-media.addEventListener("click", function(){
+media.addEventListener("click", function () {
     uploader.open({ multi: true }).then(files => {
         if (files.length === 0) {
             console.log('No files selected.')
@@ -58,57 +64,90 @@ media.addEventListener("click", function(){
     });
 })
 
+//CHANGE BLOG CHOICE
+//grab the blog that we want to adjust
+//add text box based on which blog we choose
+//if text box === clicked => 
+//
+blogButton.addEventListener("click", function(){
+    const blogs = blogSelector.selectedIndex
+    console.log(blogChoice[blogs])
+
+})
+
+//ADD ACCORDION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 //BLOG CONTENT ONE STYLING
-function changeBlogContentOne(){
-   //TEXT DECORATION
-    //Bold text
-    bold.addEventListener("click", function(){
-        blogContent.style.fontWeight = "bold"
+function changeBlogContentOne() {
+    //change header size
+    headerFont.addEventListener("click", function() {
+        const fonts = headers.selectedIndex
+        console.log(headerArray[fonts])
+        //change the style to the variable 
+        blogContent.style.fontSize = headerArray[fonts]
     })
 
-    bold.addEventListener("dblclick", function(){
+    //TEXT DECORATION
+    //Bold text
+    bold.addEventListener("click", function () {
+        // const focus = blogContent.select()
+        blogContent.style.fontWeight = "bold"
+    })
+    //turn off bold text
+    bold.addEventListener("dblclick", function () {
         blogContent.style = "font-style: normal"
     })
 
     //Italicize text
-    italic.addEventListener("click", function(){
+    italic.addEventListener("click", function () {
         blogContent.style = "font-style: italic"
     })
-
-    italic.addEventListener("dblclick", function(){
+    //turn off italics
+    italic.addEventListener("dblclick", function () {
         blogContent.style = "font-style: normal"
     })
 
     //Underline text
-    underline.addEventListener("click", function(){
+    underline.addEventListener("click", function () {
         blogContent.style = "text-decoration-line: underline"
     })
-
-    underline.addEventListener("dblclick", function(){
+    //turn off underline
+    underline.addEventListener("dblclick", function () {
         blogContent.style = "font-style: normal"
     })
 
-
     //font change
-    fontChange.addEventListener("click", function(){
+    fontChange.addEventListener("click", function () {
         const textFamily = fontFamily.selectedIndex
         console.log(textFamily)
         //change the style to the variable 
-         blogContent.style.fontFamily = fontArray[textFamily]
+        blogContent.style.fontFamily = fontArray[textFamily]
     })
 }
 
 changeBlogContentOne()
 
-
-
-
-
-
 pageChange.addEventListener("click", function () {
     location.href = "/"
 })
-
 
 function addBlog(event) {
     event.preventDefault()
@@ -129,11 +168,12 @@ function addBlog(event) {
         },
         body: JSON.stringify(newBlog)
     })
-    location.href = "/"
     resetText()
+    location.href = "/"
     //run function to display to page
 }
 
+//reset the text after blog is submitted
 function resetText() {
     blogTitle.value = ""
     blogContent.value = ""
